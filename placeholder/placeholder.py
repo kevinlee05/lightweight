517dae3d7a0a8e1459e3f1623361f42c79b9211f
kevinlee05 <kevinlee05@gmail.com>
2018-01-22 15:48:04 +0000

    imageform with generator function

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -21,13 +21,37 @@ settings.configure(
     ),
 )
 
+from io import BytesIO
+from PIL import Image
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+
+class ImageForm(forms.Form):
+    #Form to validate requested placeholder image
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate(self, image_format='PNG'):
+        #generate an image of the given type and return as raw bytes.
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
 
 def placeholder(request, width, height):
     # TODO: Restof the view will go here
-    return HttpResponse('Ok')
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
     return HttpResponse('Hello World')
