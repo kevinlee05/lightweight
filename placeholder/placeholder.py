1aeb727fa443eedcc63dc5b874547f7975c9475f
kevinlee05 <kevinlee05@gmail.com>
2018-01-22 17:17:27 +0000

    imageDraw, add cacheing

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import sys
 
@@ -22,11 +23,13 @@ settings.configure(
 )
 
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageDraw
 from django import forms
 from django.conf.urls import url
+from django.core.cache import cache
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import etag
 
 
 class ImageForm(forms.Form):
@@ -38,12 +41,28 @@ class ImageForm(forms.Form):
         #generate an image of the given type and return as raw bytes.
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
-        image = Image.new('RGB', (width, height))
-        content = BytesIO()
-        image.save(content, image_format)
-        content.seek(0)
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
         return content
 
+def generate_etag(request, width, height):
+    content = 'Placeholder: {0} x {1}'.format(width, height)
+    return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag)
 def placeholder(request, width, height):
     # TODO: Restof the view will go here
     form = ImageForm({'height': height, 'width': width})
