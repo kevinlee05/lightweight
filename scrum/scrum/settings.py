bb245014142167472f601630bd11e10ff849b85e
kevinlee05 <kevinlee05@gmail.com>
2018-01-27 16:36:00 +0800

    install rest framework, create Sprint and Task models

--- scrum/scrum/settings.py
+++ scrum/scrum/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # Internal apps
+    'board',
 )
 
 MIDDLEWARE_CLASSES = (
--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
 )
