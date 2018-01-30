b163372bcc7af82ca2c08f70676ded9db42878bf
kevinlee05 <kevinlee05@gmail.com>
2018-01-31 00:44:47 +0800

    initial django backbone setup

--- scrum/scrum/urls.py
+++ scrum/scrum/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 from board.urls import router
@@ -11,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name='board/index.html')),
 )
