414bb0c9ef4fced7b8519b619563791c47d97f21
kevinlee05 <kevinlee05@gmail.com>
2018-02-01 06:35:03 +0800

    add login View

--- scrum/board/static/board/js/models.js
+++ scrum/board/static/board/js/models.js
@@ -12,7 +12,7 @@
         var cookieValue = null;
         if (document.cookie && document.cookie != ''){
             var cookies = document.cookie.split(';');
-            for (var i==; i< cookies.length; i++){
+            for (var i=0; i< cookies.length; i++){
                 var cookie = $.trim(cookies[i]);
                 // Does this cookie string begin with the name we want?
                 if (cookie.substring(0, name.length + 1)== (name + '=')){
--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -1,11 +1,12 @@
 (function ($, Backbone, _, app){
 
-    var HomepageView = Backbone.View.extend({
-        templateName: '#home-template',
+
+    var TemplateView = Backbone.View.extend({
+        templateName: '',
         initialize: function(){
             this.template = _.template($(this.templateName).html());
         },
-        render: function(){
+        render: function () {
             var context = this.getContext(),
                 html = this.template(context);
             this.$el.html(html);
@@ -15,6 +16,16 @@
         }
     });
 
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template'
+    })
+
+    var LoginView = TemplateView.extend({
+        id: 'login',
+        templateName: '#login-template'
+    })
+
     app.views.HomepageView = HomepageView;
+    app.views.LoginView = LoginView;
 
 })(jQuery, Backbone, _, app);
