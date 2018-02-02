eb55204dea22e763a5c373b163f7e57e8b8c93c0
kevinlee05 <kevinlee05@gmail.com>
2018-02-02 22:20:45 +0800

    add FormView and extend to create enhanced LoginView

--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -16,11 +16,74 @@
         }
     });
 
+    var FormView = TemplateView.extend({
+        events: {
+            'submit form': 'submit'
+        },
+        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+        cleanErrors: function(){
+            $('.error', this.form).remove();
+        },
+        showErrors: function(errors){
+            _.map(errors, function(fieldErrors, name){
+                var field = $(':input[name=' + name + ']', this.form),
+                    label = $('label[for=' + field.attr('id') + ']', this.form);
+                if (label.length === 0) {
+                    label = $('label', this.form).first();
+                }
+                function appendError(msg) {
+                    label.before(this.errorTemplate({msg: msg}));
+                }
+                _.map(fieldErrors, appendError, this);
+            }, this);
+        },
+        serializeForm: function(form){
+            return _.object(_.map(form.serializeArray(), function(item){
+                // Convert object to tuple of (name, value)
+                return [item.name, item.value];
+            }));
+        },
+        submit: function(event){
+            event.preventDefault();
+            this.form = $(event.currentTarget);
+            this.clearErrors();
+        },
+        failure: function(xhr, status, error){
+            var errors = xhr.responseJSON;
+            this.showErrors(errors);
+        },
+        done: function(event){
+            if(event){
+                event.preventDefault();
+            }
+            this.trigger('done');
+            this.remove();
+        }
+    })
+
     var HomepageView = TemplateView.extend({
         templateName: '#home-template'
     })
 
-    var LoginView = TemplateView.extend({
+    var LoginView = FormView.extend({
+        id:'login',
+        templateName:'#login-template',
+        submit: function(event){
+            var data = {};
+            FormView.prototype.submit.apply(this, arguments);
+            data = this.serializeForm(this.form);
+            $.post(app.apiLogin, data)
+                .success($.proxy(this.loginSuccess, this))
+                .fail($.proxy(this.failure, this));
+        },
+        loginSuccess: function(data){
+            app.session.save(data.token);
+            this.done();
+        }
+    })
+
+    // old version of LoginView before inheritance of FormView
+    var LoginViewOld = TemplateView.extend({
         id: 'login',
         templateName: '#login-template',
         errorTemplate: _.template('<span class="error"><%- msg %></span>'),
